581e60ab8f81a2508ed0f6c1c7bd575b7ede313d
nullerh <80833969+nullerh@users.noreply.github.com>
2023-04-28 17:10:55 +0200

    Update 6

--- detr-main/main.py
+++ detr-main/main.py
@@ -2,7 +2,7 @@
 import argparse
 import datetime
 import json
-#import os
+import os
 import random
 import time
 from pathlib import Path
@@ -85,7 +85,7 @@ def get_args_parser():
     #parser.add_argument('--coco_panoptic_path', type=str)
     #parser.add_argument('--remove_difficult', action='store_true')
 
-    parser.add_argument('--output_dir', default='/detr-main/',
+    parser.add_argument('--output_dir', default=os.getcwd(),
                         help='path where to save, empty for no saving')
     parser.add_argument('--device', default='cuda',
                         help='device to use for training / testing')
