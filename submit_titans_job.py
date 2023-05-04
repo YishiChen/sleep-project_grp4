90b15f64b844cde566dda1d11c7121d0c96b90af
nullerh <80833969+nullerh@users.noreply.github.com>
2023-05-04 17:01:42 +0200

    6gpujob

--- submit_titans_job.py
+++ submit_titans_job.py
@@ -8,7 +8,7 @@ JOBS = [
         "partition": "titans",
         "reservation": "comp-gpu04",  # This is my GPU node, comment this line and remove line 27, if you wish to send the job out to all nodes
         "time": "4-00:00:00",  # Days-Hours:Minutes:Seconds
-        "ncpus": 8,  # Number of CPU cores
+        "ncpus": 24,  # Number of CPU cores
         "gpus": 6,  # Number of GPUs
         "memory": "64G",  # This is total RAM, change this accordingly to use
         "command": "python -m torch.distributed.launch detr-main/main.py",
