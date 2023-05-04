14814a126edaa5c34953da0709d8ec5f8a8d7715
nullerh <80833969+nullerh@users.noreply.github.com>
2023-05-04 16:25:19 +0200

    350 subject job fix box assert

--- submit_titans_job.py
+++ submit_titans_job.py
@@ -4,13 +4,13 @@ import tempfile
 # fmt: off
 JOBS = [
     {
-        "jobname": "dep_350",
+        "jobname": "dep_4",
         "partition": "titans",
         "reservation": "comp-gpu04",  # This is my GPU node, comment this line and remove line 27, if you wish to send the job out to all nodes
         "time": "4-00:00:00",  # Days-Hours:Minutes:Seconds
         "ncpus": 8,  # Number of CPU cores
-        "gpus": 2,  # Number of GPUs
-        "memory": "128G",  # This is total RAM, change this accordingly to use
+        "gpus": 4,  # Number of GPUs
+        "memory": "256G",  # This is total RAM, change this accordingly to use
         "command": "python -m torch.distributed.launch detr-main/main.py",
         "log_path": "/scratch/s194277/new_hyper"  # Usually this is your scratch space
     },
