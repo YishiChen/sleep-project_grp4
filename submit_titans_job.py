798b2fbe578a49ea46b166527c79838581efd9b2
YishiChen <yishdk@gmail.com>
2023-05-05 03:48:05 +0200

    lol

--- submit_titans_job.py
+++ submit_titans_job.py
@@ -8,9 +8,9 @@ JOBS = [
         "partition": "titans",
         "reservation": "comp-gpu04",  # This is my GPU node, comment this line and remove line 27, if you wish to send the job out to all nodes
         "time": "4-00:00:00",  # Days-Hours:Minutes:Seconds
-        "ncpus": 4,  # Number of CPU cores
-        "gpus": 1,  # Number of GPUs
-        "memory": "128G",  # This is total RAM, change this accordingly to use
+        "ncpus": 8,  # Number of CPU cores
+        "gpus": 2,  # Number of GPUs
+        "memory": "200G",  # This is total RAM, change this accordingly to use
         "command": "python -m torch.distributed.launch --nproc_per_node=1 --use_env detr-main/main.py",
         "log_path": "/scratch/s203877"  # Usually this is your scratch space
 
